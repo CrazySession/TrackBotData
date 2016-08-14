56de0b1ca01ed3be50129ad08b5847c73999cba7
Andy <coding@highyieldgaming.com>
2016-08-14 23:49:21 +0200

    specified print command ---- only selected data will be shown - (Last 15 matches yet)

--- fileReader.py
+++ fileReader.py
@@ -8,10 +8,27 @@ url = 'https://trackobot.com/profile/history.json?username=dawn-nightblade-5831&
 response = requests.get(url)
 response.raise_for_status()
 
+'''urlTwo = 'https://trackobot.com/profile?page=2.json?username=dawn-nightblade-5831&token=HfGxFHW6hbK46NkBZW55'
+responseTwo = requests.get(urlTwo)
+responseTwo.raise_for_status()'''
+
 #Load JSON
 gameStats = json.loads(response.text)
+#gameStatsTwo = json.loads(responseTwo.text)
 
 
-print(gameStats)
+w = gameStats['history']
+#e = gameStatsTwo['history']
+
+L = len(w)
+
+for i in range(L):
+    print(w[i]['mode'] , w[i]['hero'], w[i]['hero_deck'], w[i]['opponent'],
+      w[i]['opponent_deck'], w[i]['coin'], w[i]['result'],w[i]['rank'])
+
+'''for j in range(len(e)):
+    print(e[j]['mode'] , e[j]['hero'], e[j]['hero_deck'], e[j]['opponent'],
+          e[j]['opponent_deck'], e[j]['coin'], e[j]['result'],e[j]['rank'])
+          '''
 
 
