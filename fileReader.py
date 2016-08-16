f454548b9f66be2f27b8d80decab377029a8ac00
Andy <coding@highyieldgaming.com>
2016-08-16 11:10:13 +0200

    Tidied up the code - rdy to add Loop to load all stats pages from trackbot

--- fileReader.py
+++ fileReader.py
@@ -1,34 +1,26 @@
 #! python 3
 # --- JSON File Reader ---
 
-import json, requests, sys, pprint
+import json, requests, bs4
+# deleted sys and pprint
+
+pageNo = '&page=1'
 
 #Downloading Data from trackobot.com API
-url = 'https://trackobot.com/profile/history.json?username=dawn-nightblade-5831&token=HfGxFHW6hbK46NkBZW55'
+url = 'https://trackobot.com/profile/history.json?username=dawn-nightblade-5831&token=HfGxFHW6hbK46NkBZW55&page=1' + pageNo
 response = requests.get(url)
 response.raise_for_status()
 
-'''urlTwo = 'https://trackobot.com/profile?page=2.json?username=dawn-nightblade-5831&token=HfGxFHW6hbK46NkBZW55'
-responseTwo = requests.get(urlTwo)
-responseTwo.raise_for_status()'''
-
 #Load JSON
 gameStats = json.loads(response.text)
-#gameStatsTwo = json.loads(responseTwo.text)
 
 
 w = gameStats['history']
-#e = gameStatsTwo['history']
 
 L = len(w)
 
 for i in range(L):
-    print(w[i]['mode'] , w[i]['hero'], w[i]['hero_deck'], w[i]['opponent'],
+    print(w[i]['id'] , w[i]['mode'] , w[i]['hero'], w[i]['hero_deck'], w[i]['opponent'],
       w[i]['opponent_deck'], w[i]['coin'], w[i]['result'],w[i]['rank'])
-
-'''for j in range(len(e)):
-    print(e[j]['mode'] , e[j]['hero'], e[j]['hero_deck'], e[j]['opponent'],
-          e[j]['opponent_deck'], e[j]['coin'], e[j]['result'],e[j]['rank'])
-          '''
 
 
