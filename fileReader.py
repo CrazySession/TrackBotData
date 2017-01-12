eed2b29edc164438ae24ff64b548a1c5027b4310
Andy <coding@highyieldgaming.com>
2017-01-12 14:08:18 +0100

    tidying up no important code changes

--- fileReader.py
+++ fileReader.py
@@ -19,7 +19,6 @@ rowH = 1
 rowI = 1
 rowJ = 1
 sheetRow = 1
-checkRow = 0 # row for check if dl is necessary 2.4
 
 #1.0Change Dir to Save Folder / Create Save Folder
 currentDir = os.getcwd()
@@ -32,6 +31,8 @@ except FileNotFoundError:
 
 #2.0Downloading Data from trackobot.com API
 while True:
+    checkRow = 0  # row for check if dl is necessary 2.4 --- needs to be reseted evry cycle
+
     pageNo = '&page=' + str(no)
     url = 'https://trackobot.com/profile/history.json?username=dawn-nightblade-5831&token=HfGxFHW6hbK46NkBZW55' + pageNo
     response = requests.get(url)
@@ -69,7 +70,7 @@ while True:
     currentID = sheet.cell(row = sheetRow, column = 1).value
 
     if currentID == w[checkRow]['id']:
-        print('Now new games yet. Now need to update the spreadsheet!')
+        print('No new games yet. No need to update the spreadsheet!')
         break
     print(str(sheetRow)+ ' ' + str(checkRow))
 
@@ -78,8 +79,46 @@ while True:
         if (w[i]['mode'] == 'ranked'):
             sheet['A'+str(rowA)] = w[i]['id']
             rowA += 1
+
+            sheet['B' + str(rowB)] = w[i]['mode']
+            rowB += 1
+
+            sheet['C' + str(rowC)] = w[i]['hero']
+            rowC += 1
+
+            sheet['D' + str(rowD)] = w[i]['hero_deck']
+            rowD += 1
+
+            sheet['E' + str(rowE)] = w[i]['opponent']
+            rowE += 1
+
+            sheet['F' + str(rowF)] = w[i]['opponent_deck']
+            rowF += 1
+
+            sheet['G' + str(rowG)] = w[i]['coin']
+            rowG += 1
+
+            sheet['H' + str(rowH)] = w[i]['result']
+            rowH += 1
+
+            sheet['I' + str(rowI)] = w[i]['rank']
+            rowI += 1
+
+            sheet['J' + str(rowJ)] = w[i]['added']
+            rowJ += 1
+
+            currentID = sheet.cell(row=sheetRow, column=1).value
+            print(str(currentID) + ' ' + str(checkRow))
+
+            checkRow += 1
             sheetRow += 1
 
+            currentID = sheet.cell(row=sheetRow, column=1).value
+            print(str(currentID) + ' ' + str(checkRow))
+            if checkRow == 15 or currentID == w[checkRow]['id']:
+                print('Work Done!')
+                break
+    '''
     for i in range(L):
         if (w[i]['mode'] == 'ranked'):
             sheet['B'+str(rowB)] = w[i]['mode']
@@ -124,6 +163,7 @@ while True:
         if (w[i]['mode'] == 'ranked'):
             sheet['J'+str(rowJ)] = w[i]['added']
             rowJ += 1
+    '''
 
     wb.save('August_2016.xlsx')
 
